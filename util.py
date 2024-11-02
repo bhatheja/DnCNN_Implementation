8109f792ba80ec511ad17e110992af83134e6d94
Rahul <cultivateskill.rb@gmail.com>
2024-11-02 21:44:50 +0530

    Update util.py

--- util.py
+++ util.py
@@ -220,7 +220,9 @@ def psnr_values(dataloader, model, device, epoch):
         if epoch!=0:
             noisy_image = np.array(create_vertical_and_horizontal_patches(noisy_image))
             noisy_image = torch.from_numpy(noisy_image).permute(0,3,1,2).float().to(device)
+            print(noisy_image.shape)
             noisy_image = torch.nn.functional.pad(noisy_image, (1, 1, 1, 1, 0, 0, 0, 0), value=0)
+            print(noisy_image.shape)
             output = model(noisy_image)
             output = output[:, :, 1:-1, 1:-1]
             output = output.permute(0,2,3,1).detach().cpu().numpy()
@@ -230,4 +232,7 @@ def psnr_values(dataloader, model, device, epoch):
             psnr.append(cal_psnr_numpy(noisy_image, clean_image))
     return psnr
         
-    
+
+# def image_out(dataloader, model, device):
+#     for ite, (clean_image, noisy_image, mask) in enumerate(dataloader):
+        
